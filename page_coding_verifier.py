3c3500fd27fe90425a1b938a6d7dc727209f6f40
Dio Xie <dio_xie@allyes.com>
2014-12-11 17:07:39 +0800

    page_coding_verifier.py works

--- page_coding_verifier.py
+++ page_coding_verifier.py
@@ -1,16 +1,68 @@
 #!/usr/bin/env python
+# localhost:8000/add_filter/{"request_url":"abc","advertiser_id":"123"}
 
+import os
+import posixpath
+import BaseHTTPServer
+import urllib
+import cgi
+import shutil
+import mimetypes
 import sys
+
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
+def normalize_path(path):
+    # abandon query parameters
+    path = path.split('?', 1)[0]
+    path = path.split('#', 1)[0]
+    path = posixpath.normpath(urllib.unquote(path))
+    return path.strip()
+
+def get_path_sec(path, index):
+    """
+    Paras:
+        path  The path seperated with /
+        index The section index, starting from 1.
+
+    Note: The first char of 'path' will set to be / forcedly!
+        And if 'index' is out of range, return None.
+
+    E.g.,
+        get_path_sec('/abc/def', 0) ==> ''
+        get_path_sec('/abc/def', 1) ==> 'abc'
+        get_path_sec('/abc/def', 2) ==> 'def'
+        get_path_sec('/abc/def', 3) ==> None
+
+    """
+
+    if len(path) == 0:
+        return None
+
+    # make sure begin with /
+    if path[0] != '/':
+        path = '/' + path
+
+    sec_list = path.split('/')
+    if 0 <= index < len(sec_list):
+        return sec_list[index]
+    else:
+        return None
 
 class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-        print '===== self.path: ' + str(self.path)
-        print '===== translate path: ' + str(self.translate_path(self.path))
+        normalized_path = normalize_path(self.path)
+        action = get_path_sec(normalized_path, 1)
+        action_json = get_path_sec(normalized_path, 2)
 
-        f = "[1, 2, 3]"
+        print ''
+        print '=== self.path          : ' + str(self.path)
+        print '=== Normalized path    : ' + normalized_path
+        print '=== Action : ' + str(action)
+        print '=== Action Json: ' + str(action_json)
+
+        f = self.handle_action(action, action_json)
 
         self.send_response(200)
         encoding = sys.getfilesystemencoding()
@@ -19,6 +71,8 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(f)
 
+    def handle_action(self, action, action_json):
+        return '[2, 3, 4]'
 
 #
 # run
@@ -29,10 +83,9 @@ def run(HandlerClass = MyHTTPRequestHandler,
         protocol = "HTTP/1.0"):
     """Test the HTTP request handler class.
 
-      This runs an HTTP server on port 8000 (or the first command line
-      argument).
+    This runs an HTTP server on port 8000 (or the first command line argument).
 
-      """
+    """
 
     if sys.argv[1:]:
         port = int(sys.argv[1])
