b0a117abdaa8d89162e3a2d8f2dd36c45798eb8a
Dio Xie <dio_xie@allyes.com>
2014-12-12 16:48:29 +0800

    new file: two.py

---
+++ two.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python -u
+
+import math as foobar
+
+print 'La la la ...',
+print 'I ma Dio Xie.'
+print foobar.sqrt(9)
+
+x = 2
+
+if x != 2:
+    print 'x != 2'
+else:
+    pass
+    print 'x == 2'
+
