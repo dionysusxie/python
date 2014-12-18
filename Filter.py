228b6b4e28f63efd4bb36046962b4c97624b2685
Dio Xie <dio_xie@allyes.com>
2014-12-18 15:42:15 +0800

    modify Filter.py

--- Filter.py
+++ Filter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python -u
+from logging import Filter
 
 __metaclass__ = type
 
@@ -21,5 +22,30 @@ print f.filter([1, 2, 3])
 
 s = SPAM_Filter()
 s_list = ['SPAM', 'SPAM', 'ONE', 'spam']
-print s_list
-print s.filter(s_list)
+print 'Origin list:', s_list
+print 'List after filtering with SPAM:', s.filter(s_list)
+
+
+# issubclass()
+if issubclass(SPAM_Filter, Filter):
+    print 'SPAM_Filter is a subclass of Filter.'
+else:
+    print 'SPAM_Filter is NOT a subclass of Filter.'
+
+# __bases__
+print 'Superclasses of SPAM_Filter: ' + str(SPAM_Filter.__bases__)
+
+
+#
+# isinstance()
+#
+
+def isinstanceTest(obj, class_tested):
+    if isinstance(obj, class_tested):
+        print 'Tested obj is a instance of class ' + str(class_tested)
+    else:
+        print 'Tested obj is NOT a instance of class ' + str(class_tested)
+
+isinstanceTest(s, Filter)
+isinstanceTest(s, SPAM_Filter)
+
