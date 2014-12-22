1c51c1af0751655c5268ea56b31968864a75b9f5
Dio Xie <dio_xie@allyes.com>
2014-12-22 12:11:44 +0800

    new file: use_str.py

---
+++ use_str.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python -u
+
+import math
+import string
+from string import Template
+
+print 'Pi with three decimals: %.3f' % math.pi
+
+s = Template('$x, glorious $x!')
+print s.substitute(x = 'China')
+
+s = Template("It's ${x}tastic!")
+print s.substitute(x = 'slurm')
+
+s = Template('Make $$ selling $x!')
+print s.substitute(x = 'slurm')
+
+s = Template('A $thing must never $action.')
+d = {
+     'thing':   'gentleman',
+     'action':  'show his socks'
+}
+print s.substitute(d)
+
+print
+
+print '%s plus %s equals %s' % (1, 1, 2)
+
+print 'dec: %d, oct: %o, hex: %x %X, e: %e %E' % (10, 10, 10, 10, 10.0, 10.0)
+
+# width
+print
+print 'PI:%10.2f' % math.pi
+print 'PI:%010.2f' % math.pi
+print 'PI:%-10.2f#' % math.pi
+print 'PI:%1.2f#' % math.pi
+print 'My name:%10s' % 'DioXie'
+print 'My name:%-10s#' % 'DioXie'
+print 'My name:%10.4s#' % 'DioXie'
+print 'My name:%1.4s#' % 'DioXie'
+print '%.*s' % (5, 'Guido van Rossum')
+print '%+5d' % 10 + '\n%+5d' % -10
+
+# string class Variable
+print string.digits
+print string.ascii_letters
+print string.ascii_lowercase
+print string.ascii_uppercase
+print string.punctuation
+print string.printable
+
+# string.join()
+print
+seq = ['1', '2', '3']
+print ' + '.join(seq) + ' = ' + str(sum([1,2,3]))
+dirs = '', 'usr', 'bin', 'env'
+print '/'.join(dirs)
+print 'C:' + '\\'.join(dirs)
+
+# string.split()
+print
+print '1+2+3+4+5'.split('+')
+print '/usr/bin/env'.split('/')
+print 'Using the \t default\n.'.split()
+
+# string.lower()
+print
+print 'Trondheim Hammer Dance'.lower()
+
+# string.replace()
+print 'This is a test'.replace('is', 'eez')
+
+# string.strip(s, chars)
+print '  \t I am Xieliang.  \n\t'.strip() + '#'
+print '*** SPAM * for * everyone!!! ***'.strip(' *!') + '#'
+
