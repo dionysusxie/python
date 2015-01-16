3079271bd855fd5393da964526385a317fb2f671
Dio Xie <dio_xie@allyes.com>
2015-01-16 11:36:32 +0800

    modify logs/log_test.py

--- logs/log_test.py
+++ logs/log_test.py
@@ -4,13 +4,18 @@ import logging
 import my_module
 
 def main():
-    logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
+    logging.basicConfig(filename='example.log',
+                        filemode='w',
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
 
     logging.info('Started')
     logging.debug('This is a log of level DEBUG.')
     logging.info('This is a log of level INFO.')
     logging.warning('This is a log of level WARNING.')
     logging.warn('This is a log of level WARN.')
+    logging.info('%s before %s!', 'Look', 'leap')
 
     my_module.do_something()
     logging.info('Finished')
